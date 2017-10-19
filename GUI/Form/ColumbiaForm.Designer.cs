0efd87bd164adc0123466389856331fcaba8cef8
Edin Jašarević <edinjasar14@gmail.com>
2017-10-19 16:59:34 +0200

    create columbia form

---
+++ GUI/Form/ColumbiaForm.Designer.cs
@@ -0,0 +1,118 @@
+#region Copyright Syncfusion Inc. 2001 - 2017
+// Copyright Syncfusion Inc. 2001 - 2017. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+namespace Jasarsoft.ColumbiaGUI
+{
+    partial class ColumbiaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textboxDescription = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            this.buttonClose = new Syncfusion.Windows.Forms.ButtonAdv();
+            ((System.ComponentModel.ISupportInitialize)(this.textboxDescription)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textboxDescription
+            // 
+            this.textboxDescription.BackColor = System.Drawing.Color.White;
+            this.textboxDescription.BeforeTouchSize = new System.Drawing.Size(340, 214);
+            this.textboxDescription.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
+            this.textboxDescription.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textboxDescription.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textboxDescription.FocusBorderColor = System.Drawing.Color.DodgerBlue;
+            this.textboxDescription.Location = new System.Drawing.Point(12, 21);
+            this.textboxDescription.MaxLength = 1024;
+            this.textboxDescription.Metrocolor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
+            this.textboxDescription.MinimumSize = new System.Drawing.Size(10, 6);
+            this.textboxDescription.Multiline = true;
+            this.textboxDescription.Name = "textboxDescription";
+            this.textboxDescription.ReadOnly = true;
+            this.textboxDescription.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textboxDescription.Size = new System.Drawing.Size(340, 214);
+            this.textboxDescription.Style = Syncfusion.Windows.Forms.Tools.TextBoxExt.theme.Metro;
+            this.textboxDescription.TabIndex = 1;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Metro;
+            this.buttonClose.BackColor = System.Drawing.Color.DodgerBlue;
+            this.buttonClose.BeforeTouchSize = new System.Drawing.Size(340, 56);
+            this.buttonClose.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonClose.ForeColor = System.Drawing.Color.White;
+            this.buttonClose.IsBackStageButton = false;
+            this.buttonClose.Location = new System.Drawing.Point(12, 241);
+            this.buttonClose.MetroColor = System.Drawing.Color.DodgerBlue;
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(340, 56);
+            this.buttonClose.TabIndex = 2;
+            this.buttonClose.Text = "ZATVORI";
+            // 
+            // ColumbiaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.BorderThickness = 4;
+            this.CaptionAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.CaptionBarColor = System.Drawing.Color.DodgerBlue;
+            this.CaptionBarHeight = 30;
+            this.CaptionFont = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ClientSize = new System.Drawing.Size(364, 309);
+            this.Controls.Add(this.textboxDescription);
+            this.Controls.Add(this.buttonClose);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MetroColor = System.Drawing.Color.DodgerBlue;
+            this.MinimizeBox = false;
+            this.Name = "ColumbiaForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.ShowMaximizeBox = false;
+            this.ShowMinimizeBox = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SAMP Columbia State | Opis";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.textboxDescription)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt textboxDescription;
+        private Syncfusion.Windows.Forms.ButtonAdv buttonClose;
+    }
+}

12037b0eff2ea9a4ae03e6171950ee229e034b62
Edin Jašarević <edinjasar14@gmail.com>
2017-10-21 00:09:52 +0200

    check form

--- Launcher/Form/CheckForm.Designer.cs
+++ Launcher/Form/CheckForm.Designer.cs
@@ -1,11 +1,11 @@
-#region Copyright Syncfusion Inc. 2001 - 2017
-// Copyright Syncfusion Inc. 2001 - 2017. All rights reserved.
+#region Copyright Syncfusion Inc. 2001 - 2016
+// Copyright Syncfusion Inc. 2001 - 2016. All rights reserved.
 // Use of this code is subject to the terms of our license.
 // A copy of the current license can be obtained at any time by e-mailing
 // licensing@syncfusion.com. Any infringement will be prosecuted under
 // applicable laws. 
 #endregion
-namespace Jasarsoft.ColumbiaGUI
+namespace Jasarsoft.Columbia.Launcher
 {
     partial class CheckForm
     {
@@ -37,19 +37,20 @@ namespace Jasarsoft.ColumbiaGUI
         {
             this.labelName = new Syncfusion.Windows.Forms.Tools.AutoLabel();
             this.progressLoad = new Syncfusion.Windows.Forms.Tools.ProgressBarAdv();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureLogo = new System.Windows.Forms.PictureBox();
+            this.workerFile = new System.ComponentModel.BackgroundWorker();
             ((System.ComponentModel.ISupportInitialize)(this.progressLoad)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // labelName
             // 
             this.labelName.AutoSize = false;
             this.labelName.ForeColor = System.Drawing.Color.DimGray;
-            this.labelName.Location = new System.Drawing.Point(12, 124);
+            this.labelName.Location = new System.Drawing.Point(12, 109);
             this.labelName.Name = "labelName";
             this.labelName.Size = new System.Drawing.Size(340, 22);
-            this.labelName.TabIndex = 1;
+            this.labelName.TabIndex = 8;
             this.labelName.Text = "Provjera æe uskoro zapoèeti...";
             this.labelName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -63,16 +64,16 @@ namespace Jasarsoft.ColumbiaGUI
             this.progressLoad.BackSegments = false;
             this.progressLoad.BackTubeEndColor = System.Drawing.Color.White;
             this.progressLoad.BackTubeStartColor = System.Drawing.Color.LightGray;
-            this.progressLoad.BorderColor = System.Drawing.Color.DarkGray;
+            this.progressLoad.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(149)))), ((int)(((byte)(152)))));
             this.progressLoad.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.progressLoad.CustomText = null;
             this.progressLoad.CustomWaitingRender = false;
-            this.progressLoad.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.progressLoad.FontColor = System.Drawing.Color.WhiteSmoke;
+            this.progressLoad.ForeColor = System.Drawing.Color.Firebrick;
             this.progressLoad.ForegroundImage = null;
             this.progressLoad.GradientEndColor = System.Drawing.Color.DarkRed;
             this.progressLoad.GradientStartColor = System.Drawing.Color.IndianRed;
-            this.progressLoad.Location = new System.Drawing.Point(12, 149);
+            this.progressLoad.Location = new System.Drawing.Point(12, 134);
             this.progressLoad.MultipleColors = new System.Drawing.Color[] {
         System.Drawing.Color.Empty};
             this.progressLoad.Name = "progressLoad";
@@ -80,20 +81,28 @@ namespace Jasarsoft.ColumbiaGUI
             this.progressLoad.SegmentWidth = 10;
             this.progressLoad.Size = new System.Drawing.Size(340, 22);
             this.progressLoad.Step = 1;
-            this.progressLoad.TabIndex = 2;
+            this.progressLoad.TabIndex = 7;
             this.progressLoad.ThemesEnabled = false;
             this.progressLoad.TubeEndColor = System.Drawing.Color.Black;
             this.progressLoad.TubeStartColor = System.Drawing.Color.Red;
             this.progressLoad.WaitingGradientInterval = 1;
             this.progressLoad.WaitingGradientWidth = 114;
             // 
-            // pictureBox1
+            // pictureLogo
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(340, 100);
-            this.pictureBox1.TabIndex = 12;
-            this.pictureBox1.TabStop = false;
+            this.pictureLogo.Location = new System.Drawing.Point(12, 12);
+            this.pictureLogo.Name = "pictureLogo";
+            this.pictureLogo.Size = new System.Drawing.Size(340, 85);
+            this.pictureLogo.TabIndex = 6;
+            this.pictureLogo.TabStop = false;
+            // 
+            // workerFile
+            // 
+            this.workerFile.WorkerReportsProgress = true;
+            this.workerFile.WorkerSupportsCancellation = true;
+            this.workerFile.DoWork += new System.ComponentModel.DoWorkEventHandler(this.workerFile_DoWork);
+            this.workerFile.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.workerFile_ProgressChanged);
+            this.workerFile.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.workerFile_RunWorkerCompleted);
             // 
             // CheckForm
             // 
@@ -101,17 +110,19 @@ namespace Jasarsoft.ColumbiaGUI
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.BorderColor = System.Drawing.Color.Firebrick;
-            this.BorderThickness = 4;
+            this.BorderThickness = 6;
             this.CaptionAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.CaptionBarColor = System.Drawing.Color.Firebrick;
+            this.CaptionBarHeight = 24;
             this.CaptionButtonColor = System.Drawing.Color.WhiteSmoke;
+            this.CaptionButtonHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
             this.CaptionFont = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.CaptionForeColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(364, 183);
+            this.ClientSize = new System.Drawing.Size(364, 168);
             this.ControlBox = false;
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.labelName);
             this.Controls.Add(this.progressLoad);
+            this.Controls.Add(this.pictureLogo);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -121,11 +132,15 @@ namespace Jasarsoft.ColumbiaGUI
             this.Name = "CheckForm";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
+            this.ShowMaximizeBox = false;
+            this.ShowMinimizeBox = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "SAMP Columbia RolePlay | Provjera...";
+            this.Text = "SAMP Columbia State | Provjera...";
             this.TopMost = true;
+            this.Load += new System.EventHandler(this.CheckForm_Load);
+            this.Shown += new System.EventHandler(this.CheckForm_Shown);
             ((System.ComponentModel.ISupportInitialize)(this.progressLoad)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -134,6 +149,7 @@ namespace Jasarsoft.ColumbiaGUI
 
         private Syncfusion.Windows.Forms.Tools.AutoLabel labelName;
         private Syncfusion.Windows.Forms.Tools.ProgressBarAdv progressLoad;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureLogo;
+        private System.ComponentModel.BackgroundWorker workerFile;
     }
 }
---
+++ Launcher/Form/CheckForm.cs
@@ -0,0 +1,119 @@
+#region Copyright Syncfusion Inc. 2001 - 2016
+// Copyright Syncfusion Inc. 2001 - 2016. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using Syncfusion.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Jasarsoft.Columbia.Launcher
+{
+    public partial class CheckForm : Syncfusion.Windows.Forms.MetroForm
+    {
+        private enum ErrorResult
+        {
+            None,
+            Missed,
+            Unknown,            
+            Validated
+        };
+
+        public CheckForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckForm_Load(object sender, EventArgs e)
+        {
+            this.progressLoad.Value = 0;
+            this.progressLoad.Maximum = Launcher.Name.Length - 1;
+            this.labelName.Text = "Provjera æe uskoro zapoèeti...";
+        }
+
+        private void CheckForm_Shown(object sender, EventArgs e)
+        {
+            workerFile.RunWorkerAsync();
+        }
+
+        private void workerFile_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(1000);
+            e.Result = ErrorResult.None;
+            
+            Library lib = new Library();
+            DataFile df = new DataFile();
+
+            if (df.CheckUnknown(Launcher.Name, lib.Name))
+            {
+                e.Result = ErrorResult.Unknown;
+                return;
+            }
+
+            if (df.CheckMissed(Launcher.Name))
+            {
+                e.Result = ErrorResult.Missed;
+                return;
+            }
+
+            for (int i = 0; i < Launcher.Name.Length; i++)
+            {
+                workerFile.ReportProgress(i);
+
+                if (Launcher.Valid[i])
+                    if (HashFile.GetMD5(Launcher.Name[i]) != Launcher.Hash[i])
+                    {
+                        e.Result = ErrorResult.Validated;
+                        return;
+                    }      
+            }
+        }
+
+        private void workerFile_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.progressLoad.Value = e.ProgressPercentage;
+            this.labelName.Text = String.Format("Fajl {0}/{1}: {2}", e.ProgressPercentage + 1, Launcher.Name.Length, Launcher.Name[e.ProgressPercentage]);
+        }
+
+        private void workerFile_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.Hide();
+
+            string text;
+            MessageTitle title = new MessageTitle();
+
+            if (e.Result.ToString() == ErrorResult.None.ToString())
+            {
+                text = "Columbia State mod fajlovi su adekvatni i ažurirani.";
+                MessageBoxAdv.Show(text, title.InfoMsg, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (e.Result.ToString() == ErrorResult.Unknown.ToString())
+            {
+                text = "Columbia State mod sadrži nepoznate i/ili strane fajlove.";
+                MessageBoxAdv.Show(text, title.WarningMsg, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if(e.Result.ToString() == ErrorResult.Missed.ToString())
+            {
+                text = "Columbia State mod nije potpun, neki fajlovi nedostaju.";
+                MessageBoxAdv.Show(text, title.WarningMsg, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (e.Result.ToString() == ErrorResult.Validated.ToString())
+            {
+                text = "Columbia State mod nije ažuriran na zadnju verziju.";
+                MessageBoxAdv.Show(text, title.WarningMsg, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            //GC.Collect();
+            this.Close();
+        }
+    }
+}

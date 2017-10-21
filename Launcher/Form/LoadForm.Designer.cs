5b3ba289804cb21c092e2fd034573eee32700d10
Edin Jašarević <edinjasar14@gmail.com>
2017-10-21 13:02:42 +0200

    load form

---
+++ Launcher/Form/LoadForm.Designer.cs
@@ -0,0 +1,116 @@
+#region Copyright Syncfusion Inc. 2001 - 2016
+// Copyright Syncfusion Inc. 2001 - 2016. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+namespace Jasarsoft.Columbia.Launcher
+{
+    partial class LoadForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureLogo = new System.Windows.Forms.PictureBox();
+            this.labelCopyright = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            this.workerLoad = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureLogo
+            // 
+            this.pictureLogo.Location = new System.Drawing.Point(12, 12);
+            this.pictureLogo.Name = "pictureLogo";
+            this.pictureLogo.Size = new System.Drawing.Size(340, 85);
+            this.pictureLogo.TabIndex = 3;
+            this.pictureLogo.TabStop = false;
+            // 
+            // labelCopyright
+            // 
+            this.labelCopyright.AutoSize = false;
+            this.labelCopyright.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelCopyright.ForeColor = System.Drawing.Color.Gray;
+            this.labelCopyright.Location = new System.Drawing.Point(12, 109);
+            this.labelCopyright.Name = "labelCopyright";
+            this.labelCopyright.Size = new System.Drawing.Size(340, 50);
+            this.labelCopyright.TabIndex = 6;
+            this.labelCopyright.Text = "Molimo vas saèekajte dok se Columbia State launcher uèita.\r\nZa sva pitanja, suges" +
+    "tije, kritike i slièno posjetite naš forum.\r\nSva prava zadržana od strane Columb" +
+    "ia State time i treæi strana.";
+            // 
+            // workerLoad
+            // 
+            this.workerLoad.DoWork += new System.ComponentModel.DoWorkEventHandler(this.workerLoad_DoWork);
+            this.workerLoad.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.workerLoad_RunWorkerCompleted);
+            // 
+            // LoadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BorderColor = System.Drawing.Color.Firebrick;
+            this.BorderThickness = 6;
+            this.CaptionAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.CaptionBarColor = System.Drawing.Color.Firebrick;
+            this.CaptionBarHeight = 24;
+            this.CaptionButtonColor = System.Drawing.Color.WhiteSmoke;
+            this.CaptionButtonHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            this.CaptionFont = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.CaptionForeColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(364, 168);
+            this.ControlBox = false;
+            this.Controls.Add(this.labelCopyright);
+            this.Controls.Add(this.pictureLogo);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MetroColor = System.Drawing.Color.Firebrick;
+            this.MinimizeBox = false;
+            this.Name = "LoadForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.ShowMaximizeBox = false;
+            this.ShowMinimizeBox = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SAMP Columbia State | Uèitavanje...";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LoadForm_FormClosing);
+            this.Shown += new System.EventHandler(this.LoadForm_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureLogo;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel labelCopyright;
+        private System.ComponentModel.BackgroundWorker workerLoad;
+    }
+}
---
+++ Launcher/Form/LoadForm.cs
@@ -0,0 +1,272 @@
+#region Copyright Syncfusion Inc. 2001 - 2016
+// Copyright Syncfusion Inc. 2001 - 2016. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Threading;
+using System.Drawing;
+
+using System.Text;
+using System.Windows.Forms;
+using Syncfusion.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+
+namespace Jasarsoft.Columbia.Launcher
+{
+    public partial class LoadForm : Syncfusion.Windows.Forms.MetroForm
+    {
+        MainForm mainForm;
+        
+        List<int> missedFiles;
+        List<string> unknownFiles;
+
+        private enum ErrorResult
+        {
+            None,
+            Edit,
+            Work,
+            Network,
+            Database,
+            Unknown,
+            Missed
+        };
+
+        public LoadForm()
+        {
+            InitializeComponent();
+
+            MessageBoxAdv.MessageBoxStyle = MessageBoxAdv.Style.Metro;
+            MessageBoxAdv.MetroColorTable.BackColor = Color.WhiteSmoke;
+            MessageBoxAdv.MetroColorTable.ForeColor = Color.FromArgb(52, 52, 52);
+            MessageBoxAdv.MetroColorTable.BorderColor = Color.FromArgb(17, 158, 218);
+            MessageBoxAdv.MetroColorTable.CaptionForeColor = Color.FromArgb(52, 52, 52);
+            MessageBoxAdv.MetroColorTable.CaptionBarColor = Color.FromArgb(17, 158, 218);
+        }
+
+
+        private void LoadForm_Shown(object sender, EventArgs e)
+        {
+            workerLoad.RunWorkerAsync();
+        }
+
+        private void workerLoad_DoWork(object sender, DoWorkEventArgs e)
+        {
+            mainForm = new MainForm();
+
+            Process[] process = Process.GetProcessesByName("samp-nm");
+            if (process.Length > 1)
+            {
+                e.Result = ErrorResult.Work;
+                return;
+            }
+
+            //if (Path.GetFileName(Application.ExecutablePath) != "samp-nm.exe")
+            //{
+            //    e.Result = ErrorResult.Edit;
+            //    return;
+            //}
+
+            //Process[] hostNm = Process.GetProcessesByName("host-nm");
+            //if (hostNm.Length > 0)
+            //{
+            //    foreach (Process proc in hostNm)
+            //    {
+            //        do
+            //        {
+            //            proc.Kill();
+            //        } while (proc.HasExited == false);
+            //    }
+            //}
+
+            //Process.Start("host-nm.exe");
+
+            Network net = new Network();
+            if (!net.Available())
+            {
+                e.Result = ErrorResult.Network;
+                return;
+            }
+            e.Result = ErrorResult.None;
+
+
+            BaseFile bf = new BaseFile();
+            if (bf.Read())
+            {
+                Launcher.Valid = new bool[bf.Valid.Count];
+                Launcher.Name = new string[bf.Name.Count];
+                Launcher.Size = new long[bf.Size.Count];
+                Launcher.Hash = new string[bf.Hash.Count];
+                Launcher.Link = new string[bf.Link.Count];
+
+                bf.Valid.CopyTo(Launcher.Valid);
+                bf.Name.CopyTo(Launcher.Name);
+                bf.Size.CopyTo(Launcher.Size);
+                bf.Hash.CopyTo(Launcher.Hash);
+                bf.Link.CopyTo(Launcher.Link);
+
+                e.Result = ErrorResult.None;
+            }
+            else
+            {
+                e.Result = ErrorResult.Database;
+                return;
+            }
+
+            Server s = new Server();
+            if (s.Read())
+            {
+                Launcher.Locked = s.Locked;
+                Launcher.Working = s.Working;
+                Launcher.Address = s.Address;
+                Launcher.Password = s.Password;
+                Launcher.Teamspeak = s.Teamspeak;
+                Launcher.Website = s.Website;
+                Launcher.Forum = s.Forum;
+                Launcher.YouTube = s.YouTube;
+                Launcher.Facebook = s.Facebook;
+                
+                e.Result = ErrorResult.None;
+            }
+            else
+            {
+                e.Result = ErrorResult.Database;
+                return;
+            }
+
+            
+            Library dll = new Library();
+            DataFile dataFile = new DataFile();
+            this.unknownFiles = new List<string>();
+            dataFile.ReadUnknown(Launcher.Name, dll.Name);
+            if (dataFile.UnknownFiles.Count > 0)
+            {
+                foreach (string unknown in dataFile.UnknownFiles) unknownFiles.Add(unknown);
+                e.Result = ErrorResult.Unknown;
+                return;
+            }
+
+            this.missedFiles = new List<int>();
+            dataFile.ReadMissed(Launcher.Name);
+            if (dataFile.MissedFiles.Count > 0)
+            {
+                foreach (int missed in dataFile.MissedFiles) this.missedFiles.Add(missed);
+                e.Result = ErrorResult.Missed;
+                return;
+            }  
+        }
+
+        private void workerLoad_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.Hide();
+            MessageTitle title = new MessageTitle();
+
+            if (e.Result.ToString() == ErrorResult.Work.ToString())
+            {
+                string text = "Imate veæ pokrenutu instancu Columbia State launchera.";
+                MessageBoxAdv.Show(text, title.ErrorMsg, MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                Application.Exit();
+                return;
+            }
+            else if (e.Result.ToString() == ErrorResult.Edit.ToString())
+            {
+                ProcessKiller pk = new ProcessKiller();
+                
+                pk.Samp();
+                pk.GtaSa();
+                pk.HostNm();
+
+                string text = "Columbia State launcher je neovlašteno izmjenjen!";
+                MessageBoxAdv.Show(text, title.ErrorMsg, MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                Application.Exit();
+                return;
+            }
+            else if (e.Result.ToString() == ErrorResult.Network.ToString())
+            {            
+                Network net = new Network();
+                MessageBoxAdv.Show(net.Error, title.ErrorMsg, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+                return;
+            }
+            else if (e.Result.ToString() == ErrorResult.Database.ToString())
+            {
+                string text = "Konekcija sa bazom podataka je neuspješna.\nKontaktirajte administratora na forumu ili pokušajte ponovo.";
+                MessageBoxAdv.Show(text, title.ErrorMsg, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+                return;
+            }
+            else if (e.Result.ToString() == ErrorResult.Unknown.ToString())
+            {
+                string text = String.Format("Igra sadrži {0} nepoznatih fajlova.\nDa li želite ukloniti sve strane fajlove?", this.unknownFiles.Count);
+
+                if (MessageBoxAdv.Show(text, title.ErrorMsg, MessageBoxButtons.YesNo, MessageBoxIcon.Error) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    try
+                    {
+                        foreach (string name in this.unknownFiles) File.Delete(name);
+
+                        text = "Nepoznati fajlovi su uspješno uklonjeni, pokrenite ponovo aplikaciju.";
+                        MessageBoxAdv.Show(text, title.InfoMsg, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception)
+                    {
+                        text = "Nepoznati fajlovi nisu uspješno izbrisani, uèinite ruèno uklanjanje!";
+                        MessageBoxAdv.Show(text, title.ErrorMsg, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+                Application.Exit();
+                return;
+            }
+            else if (e.Result.ToString() == ErrorResult.Missed.ToString())
+            {
+                string text = String.Format("Otkriveno je {0} nedostajeæih fajlova od igre.\nDa li želite sada skinuti potrebne fajlove?", this.missedFiles.Count);
+
+                if (MessageBoxAdv.Show(text, title.WarningMsg, MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    DownloadForm dw = new DownloadForm();
+                    foreach (int index in this.missedFiles) dw.Id.Add(index);
+                    
+                    if (dw.ShowDialog(this) == System.Windows.Forms.DialogResult.Abort)
+                    {
+                        Application.Exit();
+                        return;
+                    }
+                }
+                else
+                {
+                    Application.Exit();
+                    return;
+                }
+            }
+
+            if (Launcher.Working == false)
+            {
+                string text = "Columbia State server je trenutno u procesu nadogradnje, pokušajte kasnije!";
+                MessageBoxAdv.Show(text, title.ErrorMsg, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                Application.Exit();
+                return;
+            }
+
+            mainForm.Show();
+        }
+
+        private void LoadForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ProcessKiller pk = new ProcessKiller();
+
+            pk.Samp();
+            pk.GtaSa();
+            pk.HostNm();
+        }
+    }
+}

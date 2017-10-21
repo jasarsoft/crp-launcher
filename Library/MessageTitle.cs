f645f346353a9fa997aeb91546653a943a32b971
Edin Jašarević <edinjasar14@gmail.com>
2017-10-21 14:17:30 +0200

    lib title msg class

---
+++ Library/MessageTitle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Jasarsoft.Columbia.Library
+{
+    internal class MessageTitle
+    {
+        private string infoMsg;
+        private string errorMsg;
+        private string warningMsg;
+
+        //default contructor
+        public MessageTitle()
+        {
+            this.infoMsg = "SAMP Columbia State | Info";
+            this.errorMsg = "SAMP Columbia State | Greška";
+            this.warningMsg = "SAMP Columbia State | Upozorenje";
+        }
+
+
+        //property
+        public string InfoMsg
+        {
+            get { return this.infoMsg; }
+        }
+
+        public string ErrorMsg
+        {
+            get { return this.errorMsg; }
+        }
+
+        public string WarningMsg
+        {
+            get { return this.warningMsg; }
+        }
+    }
+}

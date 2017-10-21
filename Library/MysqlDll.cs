3cea17fbd816ddd01ea8aa01d978199d16354f2b
Edin Jašarević <edinjasar14@gmail.com>
2017-10-21 14:21:01 +0200

    lib mysql dll

---
+++ Library/MysqlDll.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Jasarsoft.Columbia.Library
+{
+    internal class MysqlDll
+    {
+        private struct DllName
+        {
+            public const string mysqlData = "MySql.Data.dll";
+            public const string mysqlWeb = "MySql.Web.dll";
+        }
+
+        private struct HashValue
+        {
+            public const string mysqlData = "7D70B3192B64539739F5353A4BE3DA4C";
+            public const string mysqlWeb = "10599DF5A694765ABEF103C5C4144887";
+        }
+
+
+        public bool Check()
+        {
+            if (!File.Exists(DllName.mysqlData)) return false;
+            if (!File.Exists(DllName.mysqlWeb)) return false;
+
+            return true;
+        }
+
+        public bool Valid()
+        {
+            if (!this.Check()) return false;
+
+            HashFile hash = new HashFile();
+
+            if (hash.GetMD5(DllName.mysqlData) != HashValue.mysqlData) return false;
+            if (hash.GetMD5(DllName.mysqlWeb) != HashValue.mysqlWeb) return false;
+
+            return true;
+        }
+    }
+}

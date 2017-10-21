57328af1eb8d39f680a4dddf334e02d4c8337eaf
Edin Jašarević <edinjasar14@gmail.com>
2017-10-21 14:17:14 +0200

    lib network class

---
+++ Library/Network.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace Jasarsoft.Columbia.Library
+{
+    public class Network
+    {
+
+        public string Error 
+        {
+            get 
+            {
+                string text;
+
+                text = "Vaša internet konekcija trenutno nije u dostupna!";
+                text += Environment.NewLine;
+                text += "Aplikacija ne može nastaviti sa radom bez ovog resursa.";
+
+                return text;
+            } 
+        }
+
+        public bool Available()
+        {
+            if (NetworkInterface.GetIsNetworkAvailable())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}

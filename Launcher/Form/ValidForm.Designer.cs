db7dd91976981408e6050543cc47553c63fae07a
Edin Jašarević <edinjasar14@gmail.com>
2017-10-21 13:44:33 +0200

    valid form

---
+++ Launcher/Form/ValidForm.Designer.cs
@@ -0,0 +1,157 @@
+#region Copyright Syncfusion Inc. 2001 - 2016
+// Copyright Syncfusion Inc. 2001 - 2016. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+namespace Jasarsoft.Columbia.Launcher
+{
+    partial class ValidForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressLoad = new Syncfusion.Windows.Forms.Tools.ProgressBarAdv();
+            this.labelName = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            this.workerFile = new System.ComponentModel.BackgroundWorker();
+            this.pictureLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.progressLoad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressLoad
+            // 
+            this.progressLoad.BackGradientEndColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
+            this.progressLoad.BackGradientStartColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
+            this.progressLoad.BackgroundStyle = Syncfusion.Windows.Forms.Tools.ProgressBarBackgroundStyles.Gradient;
+            this.progressLoad.BackMultipleColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Empty};
+            this.progressLoad.BackSegments = false;
+            this.progressLoad.BackTubeEndColor = System.Drawing.Color.White;
+            this.progressLoad.BackTubeStartColor = System.Drawing.Color.LightGray;
+            this.progressLoad.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(149)))), ((int)(((byte)(152)))));
+            this.progressLoad.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.progressLoad.CustomText = null;
+            this.progressLoad.CustomWaitingRender = false;
+            this.progressLoad.FontColor = System.Drawing.Color.WhiteSmoke;
+            this.progressLoad.ForeColor = System.Drawing.Color.Firebrick;
+            this.progressLoad.ForegroundImage = null;
+            this.progressLoad.GradientEndColor = System.Drawing.Color.DarkRed;
+            this.progressLoad.GradientStartColor = System.Drawing.Color.IndianRed;
+            this.progressLoad.Location = new System.Drawing.Point(12, 134);
+            this.progressLoad.MultipleColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Empty};
+            this.progressLoad.Name = "progressLoad";
+            this.progressLoad.ProgressStyle = Syncfusion.Windows.Forms.Tools.ProgressBarStyles.Metro;
+            this.progressLoad.SegmentWidth = 10;
+            this.progressLoad.Size = new System.Drawing.Size(340, 22);
+            this.progressLoad.Step = 1;
+            this.progressLoad.TabIndex = 3;
+            this.progressLoad.ThemesEnabled = false;
+            this.progressLoad.TubeEndColor = System.Drawing.Color.Black;
+            this.progressLoad.TubeStartColor = System.Drawing.Color.Red;
+            this.progressLoad.WaitingGradientInterval = 1;
+            this.progressLoad.WaitingGradientWidth = 114;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = false;
+            this.labelName.ForeColor = System.Drawing.Color.DimGray;
+            this.labelName.Location = new System.Drawing.Point(12, 109);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(340, 22);
+            this.labelName.TabIndex = 5;
+            this.labelName.Text = "Provjera æe uskoro zapoèeti...";
+            this.labelName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // workerFile
+            // 
+            this.workerFile.WorkerReportsProgress = true;
+            this.workerFile.WorkerSupportsCancellation = true;
+            this.workerFile.DoWork += new System.ComponentModel.DoWorkEventHandler(this.WorkerFile_DoWork);
+            this.workerFile.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.WorkerFile_ProgressChanged);
+            this.workerFile.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.WorkerFile_RunWorkerCompleted);
+            // 
+            // pictureLogo
+            // 
+            this.pictureLogo.Location = new System.Drawing.Point(12, 12);
+            this.pictureLogo.Name = "pictureLogo";
+            this.pictureLogo.Size = new System.Drawing.Size(340, 85);
+            this.pictureLogo.TabIndex = 2;
+            this.pictureLogo.TabStop = false;
+            // 
+            // ValidForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BorderColor = System.Drawing.Color.Firebrick;
+            this.BorderThickness = 6;
+            this.CaptionAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.CaptionBarColor = System.Drawing.Color.Firebrick;
+            this.CaptionBarHeight = 24;
+            this.CaptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            this.CaptionButtonHoverColor = System.Drawing.Color.OrangeRed;
+            this.CaptionFont = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.CaptionForeColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(364, 168);
+            this.ControlBox = false;
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.progressLoad);
+            this.Controls.Add(this.pictureLogo);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MetroColor = System.Drawing.Color.Firebrick;
+            this.MinimizeBox = false;
+            this.Name = "ValidForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.ShowMaximizeBox = false;
+            this.ShowMinimizeBox = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SAMP Columbia State | Provjera...";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.ValidForm_Load);
+            this.Shown += new System.EventHandler(this.ValidForm_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.progressLoad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureLogo;
+        private Syncfusion.Windows.Forms.Tools.ProgressBarAdv progressLoad;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel labelName;
+        private System.ComponentModel.BackgroundWorker workerFile;
+
+    }
+}
---
+++ Launcher/Form/ValidForm.cs
@@ -0,0 +1,175 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using Syncfusion.Windows.Forms;
+
+
+namespace Jasarsoft.Columbia.Launcher
+{
+    public partial class ValidForm : Syncfusion.Windows.Forms.MetroForm
+    {
+        //private int number;
+        private List<int> id;
+        //FileStream[] fileStream;
+        List<FileStream> fileStream;
+
+        public ValidForm()
+        {
+            InitializeComponent();
+
+            this.id = new List<int>();
+            this.DialogResult = System.Windows.Forms.DialogResult.Abort;
+        }
+
+        private void ValidForm_Load(object sender, EventArgs e)
+        {
+            this.progressLoad.Value = 0;
+            this.progressLoad.Maximum = Launcher.Name.Length - 1;
+            this.labelName.Text = "Validacija æe uskoro zapoèeti...";
+        }
+
+        private void ValidForm_Shown(object sender, EventArgs e)
+        {
+            workerFile.RunWorkerAsync();
+        }
+
+        private void WorkerFile_DoWork(object sender, DoWorkEventArgs e)
+        {
+            e.Cancel = false;
+            Thread.Sleep(1000);
+
+            Library lib = new Library();
+            DataFile df = new DataFile();
+            
+            this.fileStream = new List<FileStream>();
+
+            for (int i = 0; i < Launcher.Name.Length; i++)
+            {
+                workerFile.ReportProgress(i);
+
+                if (df.CheckUnknown(Launcher.Name, lib.Name))
+                {
+                    e.Cancel = true;
+                    return;
+                }
+
+                if (df.CheckMissed(Launcher.Name))
+                {
+                    e.Cancel = true;
+                    return;
+                }
+
+                if (!File.Exists(Launcher.Name[i]))
+                {
+                    e.Cancel = true;
+                    return;
+                }
+
+                if (Launcher.Valid[i])
+                {
+                    string hash = HashFile.GetMD5(Launcher.Name[i]);
+                    
+                    if (hash == null)
+                    {
+                        e.Cancel = true;
+                        return;
+                    }
+
+                    if (hash != Launcher.Hash[i])
+                    {
+                        this.id.Add(i);
+                        continue;
+                    }
+
+                    this.fileStream.Clear();
+                    for (int j = 0; j <= i; j++)
+                    {
+                        if (this.id.Count > 0)
+                        {
+                            bool skip = false;
+                            foreach (int index in this.id)
+                            {
+                                if (j == index)
+                                {
+                                    skip = true;
+                                    break;
+                                }
+                            }
+                            if (skip) continue;
+                        }
+                       
+                        this.fileStream.Add(new FileStream(Launcher.Name[j], FileMode.Open, FileAccess.Read, FileShare.Read));
+                    }
+                }
+            }
+        }
+
+        private void WorkerFile_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.progressLoad.Value = e.ProgressPercentage;
+            this.labelName.Text = String.Format("Fajl {0}/{1}: {2}", e.ProgressPercentage + 1, Launcher.Name.Length,  Launcher.Name[e.ProgressPercentage]);
+        }
+
+        private void WorkerFile_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.Hide();
+
+            string text;
+            MessageTitle title = new MessageTitle();
+
+            if (e.Cancelled)
+            {
+                //workerFile.CancelAsync();
+                text = String.Format("Tokom provjere došlo je do neovlaštene izmjene fajlova.\nAplikacija iz sigurnostnih razloga bit æe zatvorena, pokušajte ponovo.");
+                MessageBoxAdv.Show(text, title.ErrorMsg, MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                //Application.Exit();
+                this.DialogResult = System.Windows.Forms.DialogResult.Abort;
+                //return;
+            }
+            else if (this.id.Count > 0)
+            {
+                text = String.Format("Ukupno ima {0} fajlova za ažuriranje.\nDa li želite sada izvršiti nadogradnju istih?", this.id.Count);
+                if (MessageBoxAdv.Show(text, title.WarningMsg, MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    DownloadForm dw = new DownloadForm(true);
+                    foreach (int index in this.id) dw.Id.Add(index);
+
+                    if (dw.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+                        this.DialogResult = System.Windows.Forms.DialogResult.OK;
+                    else
+                        this.DialogResult = System.Windows.Forms.DialogResult.Abort;
+                }
+                else
+                {
+                    this.DialogResult = System.Windows.Forms.DialogResult.Abort;
+                }
+            }
+            else
+            {
+                this.DialogResult = System.Windows.Forms.DialogResult.OK;
+            }
+
+            #if DEBUG
+            Library lib = new Library();
+            if (!lib.Valid())
+            {
+                text = "Columbia State Launcher biblioteke nisu validne!";
+                MessageBoxAdv.Show(text, title.ErrorMsg, MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                this.DialogResult = System.Windows.Forms.DialogResult.Abort;
+            }
+            #endif
+                
+            this.fileStream.Clear();
+            this.fileStream = null;
+            GC.Collect();
+            this.Close();
+        }
+
+    }
+}

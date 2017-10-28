6dd90110367d9f37377e9bc714a44d9ad42e0073
Edin Jašarević <edinjasar14@gmail.com>
2017-10-28 16:28:21 +0200

    base column class

---
+++ Library/Base/BaseColumn.cs
@@ -0,0 +1,56 @@
+﻿
+namespace Jasarsoft.Columbia.Library
+{
+    internal class BaseColumn
+    {
+        private readonly string valid;
+        private readonly string name;
+        private readonly string path;
+        private readonly string size;
+        private readonly string hash;
+        private readonly string link;
+        
+
+        public BaseColumn()
+        {
+            this.valid = "valid";
+            this.name = "name";
+            this.path = "path";
+            this.size = "size";
+            this.hash = "hash";
+            this.link = "fileid";
+            
+        }
+
+
+        public string Valid
+        {
+            get { return this.valid; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+        }
+
+        public string Path
+        {
+            get { return this.path; }
+        }
+
+        public string Size
+        {
+            get { return this.size; }
+        }
+
+        public string Hash
+        {
+            get { return this.hash; }
+        }
+
+        public string Link
+        {
+            get { return this.link; }
+        }
+    }
+}

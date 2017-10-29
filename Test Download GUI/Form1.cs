507f4ce9f39106ae86430ad59aba93c82d578b2d
Edin Jašarević <edinjasar14@gmail.com>
2017-10-29 20:34:42 +0100

    download gui test

---
+++ Test Download GUI/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Download_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+            });
+            MessageBox.Show("Hi from the UI thread!");
+        }
+    }
+}

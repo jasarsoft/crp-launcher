115985fbe82708871856b689da7a7305c2d0a144
jasarsoft <edinjasar14@gmail.com>
2017-11-03 23:54:53 +0100

    entry point

---
+++ Host/Class/MainHost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Jasarsoft.ColumbiaState.Host
+{
+    static class MainHost
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}

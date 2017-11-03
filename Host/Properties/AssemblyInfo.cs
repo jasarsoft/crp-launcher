63bef13c3a55be2a714b62360025767372e336cf
jasarsoft <edinjasar14@gmail.com>
2017-11-03 23:57:30 +0100

    assembly info

---
+++ Host/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Columbia State Host")]
+[assembly: AssemblyDescription("SAMP Columbia State Host")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Columbia State Team")]
+[assembly: AssemblyProduct("host-cs.exe")]
+[assembly: AssemblyCopyright("Copyright ©  2017 Columbia State Team")]
+[assembly: AssemblyTrademark("Columbia State")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("29bdb0d1-9c47-4848-9c43-f05c04023692")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

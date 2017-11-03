c697eec185e5d7c72acd25b2abb7853784a5c8f3
jasarsoft <edinjasar14@gmail.com>
2017-11-03 23:55:09 +0100

    main form

---
+++ Host/Form/MainForm.Designer.cs
@@ -0,0 +1,124 @@
+#region Copyright Syncfusion Inc. 2001 - 2016
+// Copyright Syncfusion Inc. 2001 - 2016. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+namespace Jasarsoft.ColumbiaState.Host
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureLogo = new System.Windows.Forms.PictureBox();
+            this.workerStream = new System.ComponentModel.BackgroundWorker();
+            this.workerNM = new System.ComponentModel.BackgroundWorker();
+            this.labelCopyright = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureLogo
+            // 
+            this.pictureLogo.Location = new System.Drawing.Point(12, 12);
+            this.pictureLogo.Name = "pictureLogo";
+            this.pictureLogo.Size = new System.Drawing.Size(340, 85);
+            this.pictureLogo.TabIndex = 4;
+            this.pictureLogo.TabStop = false;
+            // 
+            // workerStream
+            // 
+            this.workerStream.WorkerSupportsCancellation = true;
+            this.workerStream.DoWork += new System.ComponentModel.DoWorkEventHandler(this.workerStream_DoWork);
+            this.workerStream.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.workerStream_RunWorkerCompleted);
+            // 
+            // workerNM
+            // 
+            this.workerNM.WorkerSupportsCancellation = true;
+            this.workerNM.DoWork += new System.ComponentModel.DoWorkEventHandler(this.workerNM_DoWork);
+            this.workerNM.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.workerNM_RunWorkerCompleted);
+            // 
+            // labelCopyright
+            // 
+            this.labelCopyright.AutoSize = false;
+            this.labelCopyright.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelCopyright.ForeColor = System.Drawing.Color.Gray;
+            this.labelCopyright.Location = new System.Drawing.Point(12, 109);
+            this.labelCopyright.Name = "labelCopyright";
+            this.labelCopyright.Size = new System.Drawing.Size(340, 50);
+            this.labelCopyright.TabIndex = 7;
+            this.labelCopyright.Text = "Molimo vas saèekajte dok se Columbia State launcher uèita.\r\nZa sva pitanja, suges" +
+    "tije, kritike i slièno posjetite naš forum.\r\nSva prava zadržana od strane Columb" +
+    "ia State time i treæi strana.";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BorderColor = System.Drawing.Color.Firebrick;
+            this.BorderThickness = 6;
+            this.CaptionAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.CaptionBarColor = System.Drawing.Color.Firebrick;
+            this.CaptionBarHeight = 24;
+            this.CaptionFont = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.CaptionForeColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(364, 168);
+            this.ControlBox = false;
+            this.Controls.Add(this.labelCopyright);
+            this.Controls.Add(this.pictureLogo);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MetroColor = System.Drawing.Color.Firebrick;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.ShowMaximizeBox = false;
+            this.ShowMinimizeBox = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SAMP Columbia State | Host";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureLogo;
+        private System.ComponentModel.BackgroundWorker workerStream;
+        private System.ComponentModel.BackgroundWorker workerNM;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel labelCopyright;
+    }
+}
---
+++ Host/Form/MainForm.cs
@@ -0,0 +1,211 @@
+using Syncfusion.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Jasarsoft.ColumbiaState.Host
+{
+    public partial class MainForm : Syncfusion.Windows.Forms.MetroForm
+    {
+        private ErrorResult errorResult;
+        private List<string> allFiles;
+        private List<FileStream> fileStream;
+
+        private enum ErrorResult
+        {
+            Process,
+            MultiInstance
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.fileStream = new List<FileStream>();
+            this.allFiles = new List<string>(Directory.GetFiles(@".\", "*.*", SearchOption.AllDirectories));
+
+            MessageBoxAdv.MessageBoxStyle = MessageBoxAdv.Style.Metro;
+            MessageBoxAdv.MetroColorTable.OKButtonBackColor = Color.Firebrick;
+            MessageBoxAdv.MetroColorTable.OKButtonForeColor = Color.WhiteSmoke;
+            MessageBoxAdv.MetroColorTable.NoButtonBackColor = Color.Firebrick;
+            MessageBoxAdv.MetroColorTable.NoButtonForeColor = Color.WhiteSmoke;
+            MessageBoxAdv.MetroColorTable.YesButtonBackColor = Color.Firebrick;
+            MessageBoxAdv.MetroColorTable.YesButtonForeColor = Color.WhiteSmoke;
+            MessageBoxAdv.MetroColorTable.CancelButtonBackColor = Color.Firebrick;
+            MessageBoxAdv.MetroColorTable.CancelButtonForeColor = Color.WhiteSmoke;
+            MessageBoxAdv.MetroColorTable.BackColor = Color.WhiteSmoke;
+            MessageBoxAdv.MetroColorTable.ForeColor = Color.Black;
+            MessageBoxAdv.MetroColorTable.BorderColor = Color.Firebrick;
+            MessageBoxAdv.MetroColorTable.CaptionForeColor = Color.WhiteSmoke;
+            MessageBoxAdv.MetroColorTable.CaptionBarColor = Color.Firebrick;
+            MessageBoxAdv.MetroColorTable.CloseButtonColor = Color.WhiteSmoke;
+            MessageBoxAdv.MetroColorTable.CloseButtonHoverColor = Color.RoyalBlue;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (Path.GetFileName(Application.ExecutablePath) != "host-cs.exe")
+            {
+                ProcessKiller pk = new ProcessKiller();
+                MessageTitle title = new MessageTitle();
+
+                pk.Samp();
+                pk.GtaSa();
+                pk.SampNm();
+
+                string text = "Columbia State Host aplikacija je neovlašteno izmjenjena!";
+                MessageBoxAdv.Show(text, title.ErrorMsg, MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                Application.Exit();
+                return;
+            }
+
+
+            if (!File.Exists("columbia.exe"))
+            {
+                ProcessKiller pk = new ProcessKiller();
+                MessageTitle title = new MessageTitle();
+
+                pk.Samp();
+                pk.GtaSa();
+                pk.SampNm();
+                
+                string text = "Columbia State glavna aplikacija ne postoji ili je preimenovana!";
+                MessageBoxAdv.Show(text, title.ErrorMsg, MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                Application.Exit();
+                return;
+            }
+
+            Process[] nmProcess = Process.GetProcessesByName("columbia");
+            if (nmProcess.Length == 0)
+            {
+                Process.Start("columbia.exe");
+                Application.Exit();
+                return;
+            }
+            
+            if (nmProcess.Length > 1)
+            {
+                ProcessKiller pk = new ProcessKiller();
+                MessageTitle title = new MessageTitle();
+
+                pk.Samp();
+                pk.GtaSa();
+                pk.SampNm();
+
+                string text = "Columbia State launcher pokrenut je sa više instanci!";
+                MessageBoxAdv.Show(text, title.ErrorMsg, MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                Application.Exit();
+                return;
+            }
+
+            Process[] hosts = Process.GetProcessesByName("host-cs");
+            if (hosts.Length == 1)
+            {
+                this.workerStream.RunWorkerAsync();
+                this.workerNM.RunWorkerAsync();
+                this.Hide();
+            }
+            else
+            {
+                ProcessKiller pk = new ProcessKiller();
+                MessageTitle title = new MessageTitle();
+
+                pk.Samp();
+                pk.GtaSa();
+                pk.SampNm();
+
+                string text = "Columbia State host ne pokreæite Vi!";
+                MessageBoxAdv.Show(text, title.ErrorMsg, MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                Application.Exit();
+                return;
+            }  
+        }
+
+        private void workerStream_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(3000);
+
+            this.fileStream.Clear();
+            foreach (string name in this.allFiles)
+            {
+                if (!File.Exists(name))
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                    
+                this.fileStream.Add(new FileStream(name, FileMode.Open, FileAccess.Read, FileShare.Read));
+            }
+        }
+
+        private void workerStream_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                Process[] process = Process.GetProcessesByName("columbia");
+                if (process.Length > 0)
+                {
+                    foreach (Process temp in process)
+                    {
+                        do
+                        {
+                            temp.Kill();
+                        } while (temp.HasExited == false);
+                    }
+                }
+                workerNM.CancelAsync();
+                return;
+            }
+
+            this.workerStream.RunWorkerAsync();
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+
+        private void workerNM_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(500);
+            Process[] process = Process.GetProcessesByName("columbia");
+            if (process.Length != 1) e.Cancel = true;
+        }
+
+        private void workerNM_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                Process[] process = Process.GetProcessesByName("gtasa");
+                if (process.Length > 0)
+                {
+                    foreach (Process temp in process)
+                    {
+                        do
+                        {
+                            temp.Kill();
+                        } while (temp.HasExited == false);
+                    }
+                }
+
+                workerStream.CancelAsync();
+                Application.Exit();
+                return;
+            }
+
+            workerNM.RunWorkerAsync();
+        }
+    }
+}

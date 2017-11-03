42e55856f23c9256e8f5c3ef89cd9910a05b6809
jasarsoft <edinjasar14@gmail.com>
2017-11-03 23:54:38 +0100

    process killer class

---
+++ Host/Class/ProcessKiller.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+
+namespace Jasarsoft.ColumbiaState.Host
+{
+    public class ProcessKiller
+    {
+        private readonly string samp;
+        private readonly string gtasa;
+        private readonly string sampnm;
+        private readonly string hostnm;
+        
+        public ProcessKiller()
+        {
+            this.samp = "gtasamp";
+            this.gtasa = "gtasa";
+            this.sampnm = "columbia";
+            this.hostnm = "host-cs";
+        }
+
+
+        private void Killer(string processName)
+        {
+            Process[] allProcess = Process.GetProcessesByName(processName);
+            if (allProcess.Length > 0)
+            {
+                foreach (Process tempProcess in allProcess)
+                {
+                    do
+                    {
+                        tempProcess.Kill();
+                    } while (tempProcess.HasExited == false);
+                }
+            }
+        }
+
+        public void Samp()
+        {
+            this.Killer(this.samp);
+        }
+
+        public void GtaSa()
+        {
+            this.Killer(this.gtasa);
+        }
+
+        public void SampNm()
+        {
+            this.Killer(this.sampnm);
+        }
+
+        public void HostNm()
+        {
+            this.Killer(this.hostnm);
+        }
+    }
+}

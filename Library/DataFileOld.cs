8f8345d303c32bec567f48e4c8e6b453c99294e4
jasarsoft <edinjasar14@gmail.com>
2017-11-06 20:45:57 +0100

    lib old files

---
+++ Library/DataFileOld.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+
+namespace Jasarsoft.Columbia.Library
+{
+    public class DataFileOld
+    {
+        List<string> files;
+
+        public DataFileOld()
+        {
+            files = new List<string>(Directory.GetFiles(@".\", "*.*", SearchOption.AllDirectories));
+        }
+
+        public bool Check(string name)
+        {
+            //name = @".\" + name;
+
+            foreach (string file in files)
+            {
+                if (name == file)
+                {
+                    if (File.Exists(name))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool Valid(string hash, string name)
+        {
+            //name = @".\" + name;
+
+            foreach (string file in files)
+            {
+                if (name == file)
+                {
+                    using (var md5 = MD5.Create())
+                    {
+                        using (var stream = File.OpenRead(file))
+                        {
+                            if (hash == BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", string.Empty))
+                                return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Library/Download.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jasarsoft.Columbia.Library
+{
+    public class Download
+    {
+        private string url = "https://mega.nz/#!0Nwi0SAD!aDb8XelO0Z72zzVOyKkT7LReu-y2TKOIwWuTYjcu7nY";
+        private string name = "eax1.dll";
+
+
+        public bool Test()
+        {
+            WebClient client = new WebClient();
+
+            client.DownloadFile(url, name);
+            client.DownloadFile("https://mega.nz/#!ERQCmQAI!aDb8XelO0Z72zzVOyKkT7LReu-y2TKOIwWuTYjcu7nY", ".\\test\\eax2.dll");
+            return true;
+        }
+
+        public bool File(string link, string path)
+        {
+            //CG.Web.MegaApiClient.MegaApiClient client = new CG.Web.MegaApiClient.MegaApiClient();
+            //client.LoginAnonymous();
+
+            //using (var fileStream = new FileStream(path, FileMode.Create))
+            //{
+            //    using (var downloadStream = new ProgressionStream(client.Download(new Uri(link)), this.PrintProgression))
+            //    {
+            //        downloadStream.CopyTo(fileStream);
+            //    }
+            //}
+
+
+            try
+            {
+                //WebClient client = new WebClient();
+                //client.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0)");
+                //client.DownloadFile(link, path);
+                return true;
+            }
+            catch (WebException)
+            {
+                return false;
+            }
+            //WebClient client = new WebClient();
+            //client.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0)");
+            //client.DownloadFile(link, path);
+            
+            //return true;
+        }
+
+        private void PrintProgression(double progression)
+        {
+            //Console.WriteLine(progression);
+        }
+        
+    }
+}

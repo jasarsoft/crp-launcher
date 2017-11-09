764ec359a12bf036260ae9e6401908e557c13743
jasarsoft <edinjasar14@gmail.com>
2017-11-09 23:30:11 +0100

    idata interface

---
+++ Launcher/Interface/IData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jasarsoft.Columbia
+{
+    interface IData
+    {
+        bool Check();
+        bool Valid();
+    }
+}

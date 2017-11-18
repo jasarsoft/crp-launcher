ca9842d5ea54cd56aa37badf01bf6f6f5eef5b72
jasarsoft <edinjasar14@gmail.com>
2017-11-18 17:25:16 +0100

    md5 hash file

---
+++ UpdateApp/HashFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+
+namespace Jasarsoft.Columbia.Update
+{
+    internal static class HashFile
+    {
+        public static string GetMD5(string fileName)
+        {
+            if (fileName == null || !File.Exists(fileName)) return null;
+
+            using (MD5 hash = MD5.Create())
+            {
+                using (FileStream stream = File.OpenRead(fileName))
+                {
+                    return BitConverter.ToString(hash.ComputeHash(stream)).Replace("-", String.Empty);
+                } 
+            }
+        }
+    }
+}

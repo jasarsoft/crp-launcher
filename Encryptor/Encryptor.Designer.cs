17b29094bd3c099dfb409f543e3807ad23a48ee7
jasarsoft <edinjasar14@gmail.com>
2017-12-09 22:52:37 +0100

    set namespace

--- Encryptor/Encryptor.Designer.cs
+++ Encryptor/Encryptor.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Encryptor
+﻿namespace Jasarsoft.StringEncryptor
 {
     partial class Encryptor
     {
--- Encryptor/Program.cs
+++ Encryptor/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
 
-namespace Encryptor
+namespace Jasarsoft.StringEncryptor
 {
     static class Program
     {

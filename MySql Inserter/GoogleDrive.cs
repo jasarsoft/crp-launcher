9bf8142ab1625405540d77eb0eb94d00b7d7a939
jasarsoft <edinjasar14@gmail.com>
2017-12-11 19:14:42 +0100

    mysql inserter google drive authenticate

---
+++ MySql Inserter/GoogleDrive.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v2;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace Jasarsoft.MySQLInserter
+{
+    public class GoogleDrive
+    {
+        public static DriveService AuthenticateService()
+        {
+
+            //Google Drive scopes Documentation:   https://developers.google.com/drive/web/scopes
+            string[] scopes = new string[] { DriveService.Scope.Drive,  // view and manage your files and documents
+                                             DriveService.Scope.DriveAppdata,  // view and manage its own configuration data
+                                             DriveService.Scope.DriveAppsReadonly,   // view your drive apps
+                                             DriveService.Scope.DriveFile,   // view and manage files created by this app
+                                             DriveService.Scope.DriveMetadataReadonly,   // view metadata for files
+                                             DriveService.Scope.DriveReadonly,   // view files and documents on your drive
+                                             DriveService.Scope.DriveScripts };  // modify your app scripts     
+
+            GoogleCredential credential;
+            try
+            {
+                using (var stream = new System.IO.FileStream("client_secrets.json", System.IO.FileMode.Open, System.IO.FileAccess.Read))
+                {
+                    credential = GoogleCredential.FromStream(stream).CreateScoped(scopes);
+                }
+                    
+                return new DriveService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Columbia State",
+                });
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("CreateServiceAccountDriveFailed", ex);
+            }
+        }
+    }
+}

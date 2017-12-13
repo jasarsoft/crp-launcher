c041c8c9ca5ff693cfc2fd9d73d9e8de2048f061
jasarsoft <edinjasar14@gmail.com>
2017-12-13 22:46:56 +0100

    clasa GoogleFile za google drive fajl kao objekat

---
+++ Launcher/Class/GDrive/GoogleFile.cs
@@ -0,0 +1,47 @@
+﻿
+namespace Jasarsoft.Columbia
+{
+    internal class GoogleFile
+    {
+        private string id;
+        private bool valid;
+        private string file;
+        private string hash;
+        private long size;
+
+
+        public GoogleFile(string id, bool valid, string file, string hash, long size)
+        {
+            this.id = id;
+            this.valid = valid;
+            this.file = file;
+            this.hash = hash;
+            this.size = size;            
+        }
+
+        public string Id
+        {
+            get { return this.id; }
+        }
+
+        public bool Valid
+        {
+            get { return this.valid; }
+        }
+
+        public string File
+        {
+            get { return this.file; }
+        }
+
+        public string Hash
+        {
+            get { return this.hash; }
+        }
+
+        public long Size
+        {
+            get { return this.size; }
+        }
+    }
+}

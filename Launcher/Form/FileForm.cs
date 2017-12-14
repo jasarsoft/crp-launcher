cfccf802b5682bfff907ce0640e6b81da7ed0f91
jasarsoft <edinjasar14@gmail.com>
2017-12-14 18:22:49 +0100

    postavka sirina kolona i property

--- Launcher/Form/FileForm.cs
+++ Launcher/Form/FileForm.cs
@@ -19,18 +19,39 @@ namespace Jasarsoft.Columbia
 {
     public partial class FileForm : Syncfusion.Windows.Forms.MetroForm
     {
-        public FileForm()
+        public FileForm(object data)
         {
             InitializeComponent();
 
+            gridListControl1.DataSource = data;
+        }
+
+        public int FileMissed
+        {
+            set { labelMissed.Text = String.Format("Nedostajuæi: {0}", value); }
+        }
+
+        public int FileUnknown
+        {
+            set { labelUnknown.Text = String.Format("Nepoznatih: {0}", value); }
+        }
+
+        public int FileIncorrect
+        {
+            set { labelIncorrect.Text = String.Format("Nispravnih: {0}", value); }
+        }
+
+        public int FileTotal
+        {
+            set { labelTotal.Text = String.Format("Ukupno sve: {0}", value); }
         }
 
         private void FileForm_Load(object sender, EventArgs e)
         {
-            //gridListControl1.Grid.ColWidths[1] = 50;
-            //gridListControl1.Grid.ColWidths[2] = 200;
-            //gridListControl1.Grid.ColWidths[3] = 50;
-            //gridListControl1.Grid.ColWidths[4] = 70;
+            gridListControl1.Grid.ColWidths[1] = 60;
+            gridListControl1.Grid.ColWidths[2] = 340;
+            gridListControl1.Grid.ColWidths[3] = 100;
+            gridListControl1.Grid.ColWidths[4] = 100;
         }
     }
 }

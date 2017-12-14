167654861d82f7e56d8047d5a2cd39aeb4af772d
jasarsoft <edinjasar14@gmail.com>
2017-12-15 00:59:50 +0100

    nova klasa za objekat fajla greske

---
+++ Launcher/Class/ErrorFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jasarsoft.Columbia
+{
+    internal class ErrorFile
+    {
+        private int number;
+        private string file;
+        private long size;
+        private string stats;
+
+        public ErrorFile(int number, string file, long size, string stats)
+        {
+            this.number = number;
+            this.file = file;
+            this.size = size;
+            this.stats = stats;
+        }
+
+        public int Number
+        {
+            get { return this.number; }
+        }
+
+        public string File
+        {
+            get { return this.file; }
+        }
+
+        public long Size
+        {
+            get { return this.size; }
+        }
+
+        public string Stats
+        {
+            get { return this.stats; }
+        }
+    }
+}

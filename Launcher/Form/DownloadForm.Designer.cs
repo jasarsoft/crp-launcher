51cd3fb39fca9c91c5b62f1fae8f5923e985e1f6
jasarsoft <edinjasar14@gmail.com>
2017-12-16 01:07:51 +0100

    novi dizajn download forme

--- Launcher/Form/DownloadForm.Designer.cs
+++ Launcher/Form/DownloadForm.Designer.cs
@@ -35,34 +35,35 @@ namespace Jasarsoft.Columbia
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureLogo = new System.Windows.Forms.PictureBox();
+            this.components = new System.ComponentModel.Container();
             this.labelName = new Syncfusion.Windows.Forms.Tools.AutoLabel();
             this.progressOne = new Syncfusion.Windows.Forms.Tools.ProgressBarAdv();
             this.progressAll = new Syncfusion.Windows.Forms.Tools.ProgressBarAdv();
             this.labelValue = new Syncfusion.Windows.Forms.Tools.AutoLabel();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).BeginInit();
+            this.buttonStart = new Syncfusion.Windows.Forms.ButtonAdv();
+            this.imageStreamerSlide = new Syncfusion.Windows.Forms.Tools.ImageStreamer();
+            this.labelErrorValue = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            this.labelTotal = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            this.labelError = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            this.labelTotalValue = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            this.buttonClose = new Syncfusion.Windows.Forms.ButtonAdv();
+            this.pictureLogo = new System.Windows.Forms.PictureBox();
+            this.workerSlide = new System.ComponentModel.BackgroundWorker();
             ((System.ComponentModel.ISupportInitialize)(this.progressOne)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.progressAll)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureLogo
-            // 
-            this.pictureLogo.Image = global::Jasarsoft.Columbia.Properties.Resources.cover;
-            this.pictureLogo.Location = new System.Drawing.Point(12, 12);
-            this.pictureLogo.Name = "pictureLogo";
-            this.pictureLogo.Size = new System.Drawing.Size(340, 85);
-            this.pictureLogo.TabIndex = 3;
-            this.pictureLogo.TabStop = false;
             // 
             // labelName
             // 
             this.labelName.AutoSize = false;
             this.labelName.ForeColor = System.Drawing.Color.DimGray;
-            this.labelName.Location = new System.Drawing.Point(12, 109);
+            this.labelName.Location = new System.Drawing.Point(12, 384);
             this.labelName.Name = "labelName";
-            this.labelName.Size = new System.Drawing.Size(234, 22);
+            this.labelName.Size = new System.Drawing.Size(514, 22);
             this.labelName.TabIndex = 6;
-            this.labelName.Text = "Naziv: ";
+            this.labelName.Text = "Molimo vas kliknite na Poèni da bih ste zapoèeli process ažuriranja potrebnih dat" +
+    "oteka...";
             this.labelName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // progressOne
@@ -84,13 +85,13 @@ namespace Jasarsoft.Columbia
             this.progressOne.ForegroundImage = null;
             this.progressOne.GradientEndColor = System.Drawing.Color.DarkRed;
             this.progressOne.GradientStartColor = System.Drawing.Color.IndianRed;
-            this.progressOne.Location = new System.Drawing.Point(12, 134);
+            this.progressOne.Location = new System.Drawing.Point(12, 409);
             this.progressOne.MultipleColors = new System.Drawing.Color[] {
         System.Drawing.Color.Empty};
             this.progressOne.Name = "progressOne";
             this.progressOne.ProgressStyle = Syncfusion.Windows.Forms.Tools.ProgressBarStyles.Metro;
             this.progressOne.SegmentWidth = 12;
-            this.progressOne.Size = new System.Drawing.Size(340, 22);
+            this.progressOne.Size = new System.Drawing.Size(640, 22);
             this.progressOne.Step = 1;
             this.progressOne.TabIndex = 7;
             this.progressOne.ThemesEnabled = false;
@@ -119,13 +120,13 @@ namespace Jasarsoft.Columbia
             this.progressAll.ForegroundImage = null;
             this.progressAll.GradientEndColor = System.Drawing.Color.DarkRed;
             this.progressAll.GradientStartColor = System.Drawing.Color.IndianRed;
-            this.progressAll.Location = new System.Drawing.Point(12, 162);
+            this.progressAll.Location = new System.Drawing.Point(12, 437);
             this.progressAll.MultipleColors = new System.Drawing.Color[] {
         System.Drawing.Color.Empty};
             this.progressAll.Name = "progressAll";
             this.progressAll.ProgressStyle = Syncfusion.Windows.Forms.Tools.ProgressBarStyles.Metro;
             this.progressAll.SegmentWidth = 12;
-            this.progressAll.Size = new System.Drawing.Size(340, 22);
+            this.progressAll.Size = new System.Drawing.Size(640, 22);
             this.progressAll.Step = 1;
             this.progressAll.TabIndex = 8;
             this.progressAll.ThemesEnabled = false;
@@ -140,12 +141,116 @@ namespace Jasarsoft.Columbia
             // 
             this.labelValue.AutoSize = false;
             this.labelValue.ForeColor = System.Drawing.Color.DimGray;
-            this.labelValue.Location = new System.Drawing.Point(252, 109);
+            this.labelValue.Location = new System.Drawing.Point(532, 384);
             this.labelValue.Name = "labelValue";
-            this.labelValue.Size = new System.Drawing.Size(100, 22);
+            this.labelValue.Size = new System.Drawing.Size(120, 22);
             this.labelValue.TabIndex = 9;
             this.labelValue.Text = "0/0 B";
             this.labelValue.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Metro;
+            this.buttonStart.BackColor = System.Drawing.Color.Firebrick;
+            this.buttonStart.BeforeTouchSize = new System.Drawing.Size(144, 36);
+            this.buttonStart.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonStart.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonStart.IsBackStageButton = false;
+            this.buttonStart.Location = new System.Drawing.Point(358, 474);
+            this.buttonStart.MetroColor = System.Drawing.Color.Firebrick;
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(144, 36);
+            this.buttonStart.TabIndex = 10;
+            this.buttonStart.Text = "Poèni";
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // imageStreamerSlide
+            // 
+            this.imageStreamerSlide.AllowDragging = true;
+            this.imageStreamerSlide.ImageStreamDirection = Syncfusion.Windows.Forms.Tools.ImageStreamer.StreamDirection.TopToBottom;
+            this.imageStreamerSlide.InternalBackColor = System.Drawing.Color.Transparent;
+            this.imageStreamerSlide.Location = new System.Drawing.Point(12, 12);
+            this.imageStreamerSlide.Name = "imageStreamerSlide";
+            this.imageStreamerSlide.Size = new System.Drawing.Size(640, 360);
+            this.imageStreamerSlide.SliderSpeed = 10000;
+            this.imageStreamerSlide.SlideShow = true;
+            this.imageStreamerSlide.TabIndex = 11;
+            this.imageStreamerSlide.TextAnimationDirection = Syncfusion.Windows.Forms.Tools.ImageStreamer.TextStreamDirection.RightToLeft;
+            this.imageStreamerSlide.Visible = false;
+            // 
+            // labelErrorValue
+            // 
+            this.labelErrorValue.AutoSize = false;
+            this.labelErrorValue.ForeColor = System.Drawing.Color.DimGray;
+            this.labelErrorValue.Location = new System.Drawing.Point(138, 474);
+            this.labelErrorValue.Name = "labelErrorValue";
+            this.labelErrorValue.Size = new System.Drawing.Size(100, 18);
+            this.labelErrorValue.TabIndex = 12;
+            this.labelErrorValue.Text = "0";
+            this.labelErrorValue.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = false;
+            this.labelTotal.ForeColor = System.Drawing.Color.DimGray;
+            this.labelTotal.Location = new System.Drawing.Point(12, 492);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(120, 18);
+            this.labelTotal.TabIndex = 13;
+            this.labelTotal.Text = "Ukupno skinuto:";
+            this.labelTotal.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = false;
+            this.labelError.ForeColor = System.Drawing.Color.DimGray;
+            this.labelError.Location = new System.Drawing.Point(12, 474);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(120, 18);
+            this.labelError.TabIndex = 14;
+            this.labelError.Text = "Ukupno grešaka:";
+            this.labelError.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelTotalValue
+            // 
+            this.labelTotalValue.AutoSize = false;
+            this.labelTotalValue.ForeColor = System.Drawing.Color.DimGray;
+            this.labelTotalValue.Location = new System.Drawing.Point(138, 492);
+            this.labelTotalValue.Name = "labelTotalValue";
+            this.labelTotalValue.Size = new System.Drawing.Size(100, 18);
+            this.labelTotalValue.TabIndex = 15;
+            this.labelTotalValue.Text = "0/0 B";
+            this.labelTotalValue.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Metro;
+            this.buttonClose.BackColor = System.Drawing.Color.Firebrick;
+            this.buttonClose.BeforeTouchSize = new System.Drawing.Size(144, 36);
+            this.buttonClose.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonClose.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonClose.IsBackStageButton = false;
+            this.buttonClose.Location = new System.Drawing.Point(508, 474);
+            this.buttonClose.MetroColor = System.Drawing.Color.Firebrick;
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(144, 36);
+            this.buttonClose.TabIndex = 16;
+            this.buttonClose.Text = "Odustani";
+            // 
+            // pictureLogo
+            // 
+            this.pictureLogo.Image = global::Jasarsoft.Columbia.Properties.Resources.logo_slide;
+            this.pictureLogo.Location = new System.Drawing.Point(12, 12);
+            this.pictureLogo.Name = "pictureLogo";
+            this.pictureLogo.Size = new System.Drawing.Size(640, 360);
+            this.pictureLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureLogo.TabIndex = 17;
+            this.pictureLogo.TabStop = false;
+            // 
+            // workerSlide
+            // 
+            this.workerSlide.DoWork += new System.ComponentModel.DoWorkEventHandler(this.workerSlide_DoWork);
+            this.workerSlide.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.workerSlide_RunWorkerCompleted);
             // 
             // DownloadForm
             // 
@@ -157,43 +262,54 @@ namespace Jasarsoft.Columbia
             this.CaptionAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.CaptionBarColor = System.Drawing.Color.Firebrick;
             this.CaptionBarHeight = 24;
-            this.CaptionButtonColor = System.Drawing.Color.Black;
-            this.CaptionButtonHoverColor = System.Drawing.Color.WhiteSmoke;
+            this.CaptionButtonColor = System.Drawing.Color.WhiteSmoke;
+            this.CaptionButtonHoverColor = System.Drawing.Color.Black;
             this.CaptionFont = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.CaptionForeColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(364, 196);
+            this.ClientSize = new System.Drawing.Size(664, 522);
+            this.Controls.Add(this.pictureLogo);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.labelTotalValue);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.labelTotal);
+            this.Controls.Add(this.labelErrorValue);
+            this.Controls.Add(this.imageStreamerSlide);
+            this.Controls.Add(this.buttonStart);
             this.Controls.Add(this.labelValue);
             this.Controls.Add(this.progressAll);
             this.Controls.Add(this.progressOne);
             this.Controls.Add(this.labelName);
-            this.Controls.Add(this.pictureLogo);
-            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MetroColor = System.Drawing.Color.Firebrick;
-            this.MinimizeBox = false;
             this.Name = "DownloadForm";
             this.ShowIcon = false;
             this.ShowMaximizeBox = false;
-            this.ShowMinimizeBox = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SAMP Columbia State | Ažuriranje...";
             this.Shown += new System.EventHandler(this.DownloadForm_Shown);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.progressOne)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.progressAll)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureLogo;
         private Syncfusion.Windows.Forms.Tools.AutoLabel labelName;
         private Syncfusion.Windows.Forms.Tools.ProgressBarAdv progressOne;
         private Syncfusion.Windows.Forms.Tools.ProgressBarAdv progressAll;
         private Syncfusion.Windows.Forms.Tools.AutoLabel labelValue;
+        private Syncfusion.Windows.Forms.ButtonAdv buttonStart;
+        private Syncfusion.Windows.Forms.Tools.ImageStreamer imageStreamerSlide;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel labelErrorValue;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel labelTotal;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel labelError;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel labelTotalValue;
+        private Syncfusion.Windows.Forms.ButtonAdv buttonClose;
+        private System.Windows.Forms.PictureBox pictureLogo;
+        private System.ComponentModel.BackgroundWorker workerSlide;
     }
 }

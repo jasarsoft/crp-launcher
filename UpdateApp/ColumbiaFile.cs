16df0bbf10aaa4b20af2f4a789580f64d8f36e26
jasarsoft <edinjasar14@gmail.com>
2017-12-17 15:20:01 +0100

    update columbia file class

---
+++ UpdateApp/ColumbiaFile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jasarsoft.Columbia.Update
+{
+    internal class ColumbiaFile
+    {
+        //private fields
+        private readonly string name;
+        private readonly string hash;
+        private readonly long size;
+
+        /// <summary>
+        /// Class default constructor
+        /// </summary>
+        public ColumbiaFile(string name, string hash, long size)
+        {
+            this.name = name;
+            this.hash = hash;
+            this.size = size;
+        }
+
+        /// <summary>
+        /// Name of update file
+        /// </summary>
+        public string Name
+        {
+            get { return this.name; }
+        }
+
+        /// <summary>
+        /// Hash value of upadate file
+        /// </summary>
+        public string Hash
+        {
+            get { return this.hash; }
+        }
+
+        /// <summary>
+        /// Size value of file
+        /// </summary>
+        public long Size
+        {
+            get { return this.size; }
+        }
+    }
+}

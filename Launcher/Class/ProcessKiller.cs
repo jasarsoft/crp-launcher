2eac4d180332f3cd58f589d7bd791faba2c48bb9
jasarsoft <edinjasar14@gmail.com>
2017-12-19 18:06:02 +0100

    nova implementacija killera

--- Launcher/Class/ProcessKiller.cs
+++ Launcher/Class/ProcessKiller.cs
@@ -1,29 +1,39 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Collections.Generic;
-
+using System.Threading;
 
 namespace Jasarsoft.Columbia
 {
     public class ProcessKiller
     {
+        private readonly string gta;
         private readonly string samp;
-        private readonly string gtasa;
-        private readonly string sampnm;
-        private readonly string hostnm;
-        
+        private readonly string host;
+        private readonly string columbia;
+
+
         public ProcessKiller()
         {
+            this.gta = "gtasa";
             this.samp = "gtasamp";
-            this.gtasa = "gtasa";
-            this.sampnm = "columbia";
-            this.hostnm = "host-cs";
+            this.host = "host-cs";
+            this.columbia = "columbia";
         }
 
 
-        private void Killer(string processName)
+        private void Kill(object processName)
         {
-            Process[] allProcess = Process.GetProcessesByName(processName);
+#if TRACE
+            Trace.TraceInformation("Ulaz u metodu Killer; ProcessKiller;");
+#endif
+            if (processName == null)
+            {
+                Trace.TraceError("Greška u argumentu processName; ProcessKiller.Kill();");
+                throw new ArgumentNullException("ProcessKiller.Kill(); Argument 'processName' ne moze biti null.");
+            }
+
+            Process[] allProcess = Process.GetProcessesByName(Convert.ToString(processName));
             if (allProcess.Length > 0)
             {
                 foreach (Process tempProcess in allProcess)
@@ -31,29 +41,53 @@ namespace Jasarsoft.Columbia
                     do
                     {
                         tempProcess.Kill();
+                        tempProcess.WaitForExit();
+#if TRACE
+                        Trace.TraceInformation("Process {0} je ubijen.", tempProcess.ProcessName);
+#endif
                     } while (tempProcess.HasExited == false);
                 }
+            }
+#if TRACE
+            else
+                Trace.TraceInformation("ProcessKiller.Kill(); Nema processa: {0}", processName);
+#endif
+
+            //dodatna provjera
+            allProcess = Process.GetProcessesByName(Convert.ToString(processName));
+            if (allProcess.Length > 0)
+            {
+                string message = "Zatvaranje procesa '" + processName + "' nije uspješno izvršeno.\n" +
+                                 "Kontaktirajte administraciju na forumu o vašoj pogrešsci";
+                throw new MethodAccessException(message);
             }
         }
 
         public void Samp()
         {
-            this.Killer(this.samp);
+            Thread thread = new Thread(Kill);
+            thread.Start(samp);
+#if TRACE
+            if (thread.IsAlive) Trace.TraceInformation("ProcessKiller.Samp(); Thread.Start(); OK");
+#endif
         }
 
-        public void GtaSa()
+        public void SanAndreas()
         {
-            this.Killer(this.gtasa);
+            Thread thread = new Thread(Kill);
+            thread.Start(this.gta);
         }
 
-        public void SampNm()
+        public void Columbia()
         {
-            this.Killer(this.sampnm);
+            Thread thread = new Thread(Kill);
+            thread.Start(this.columbia);
         }
 
-        public void HostNm()
+        public void Host()
         {
-            this.Killer(this.hostnm);
+            Thread thread = new Thread(Kill);
+            thread.Start(this.host);
         }
     }
 }

23153eea91e610a6b2ff060565cecb7751dcdbf7
jasarsoft <edinjasar14@gmail.com>
2017-12-26 16:56:14 +0100

    nova klasa ColumbiaFile

---
+++ Launcher/ColumbiaFile.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Jasarsoft.Columbia
+{
+    internal class ColumbiaFile
+    {
+        private readonly bool valid;
+        private readonly string name;
+        private readonly long size;
+        private readonly string hash;
+        private readonly string link;
+
+
+        public ColumbiaFile(bool valid, string name, long size, string hash, string link)
+        {
+            this.valid = valid;
+            this.name = name;
+            this.size = size;
+            this.hash = hash;
+            this.link = link;
+        }
+
+
+        public bool Valid
+        {
+            get { return this.valid; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+        }
+
+        public long Size
+        {
+            get { return this.size; }
+        }
+
+        public string Hash
+        {
+            get { return this.hash; }
+        }
+
+        public string Link
+        {
+            get { return this.link; }
+        }
+    }
+}
